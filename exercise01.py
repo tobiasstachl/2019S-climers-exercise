65ff65841a161e6d1b8c28c7cd6acff5248f8f89
fzaussin <felix.zaussinger@gmail.com>
2019-04-05 12:33:02 +0200

    Exercise one done

--- exercise01.py
+++ exercise01.py
@@ -25,6 +25,9 @@ register_matplotlib_converters()
 import statsmodels.api as sm
 from scipy import stats
 
+import seaborn as sns
+sns.set(context='paper', style='ticks')
+
 
 def read_data(infile, parse_col, date_str):
     """
@@ -75,10 +78,12 @@ def plot_sealevel(fn_sealevel, outpath):
     plt.figure(figsize=(12,5))
     columns = ['Glob', 'NHem', 'SHem']
     plt.plot(data[columns])
+    sns.despine()
     plt.title("Global sea-level anomalies")
     plt.ylabel("Sea level anomaly (m)")
-    plt.grid(True)
-    plt.legend(columns)
+    plt.grid(color='grey', linestyle='--', linewidth=0.5)
+    plt.legend(columns, bbox_to_anchor=(1.01, 0.5), loc="center left")
+    plt.tight_layout()
     plt.savefig(os.path.join(outpath, 'ex1_sealevel-global-anomalies-ts.png'))
 
     # plot the data
@@ -86,11 +91,110 @@ def plot_sealevel(fn_sealevel, outpath):
     columns_zonal = ['b24N.90N', 'b24S.24N', 'b90S.24S', 'b64N.90N', 'b44N.64N', 'b24N.44N',
                      'bEQU.24N', 'b24S.EQU','b44S.24S', 'b64S.44S', 'b90S.64S', 'Nino34']
     plt.plot(data[columns_zonal])
+    sns.despine()
     plt.title("Latitudinal band sea-level anomalies")
     plt.ylabel("Sea level anomaly (m)")
-    plt.grid(True)
-    plt.legend(columns_zonal)
+    plt.grid(color='grey', linestyle='--', linewidth=0.5)
+    plt.legend(columns_zonal, bbox_to_anchor=(1.01, 0.5), loc="center left")
+    plt.tight_layout()
     plt.savefig(os.path.join(outpath, 'ex1_sealevel-zonal-anomalies-ts.png'))
+
+    # aggregate to annual data
+    data_annual = data.resample('A').mean()
+
+    for col in data_annual.columns:
+        print(col)
+
+        # 1) compute trend based on ordinary least square regression
+        y = data_annual[col]
+        x = list(range(1, len(data_annual) + 1))
+        x = np.reshape(x, (-1, 1))
+        x = sm.add_constant(x)  # to add intercept
+        trd = sm.OLS(y, x).fit()  # fit regression
+        data_annual["{}_trd_ols".format(col)] = trd.predict(x)  # compute trend line
+        print(trd.summary())
+
+        # 2) compute trend based on Theil-Sen slope
+        y = data_annual[col]
+        theilsen_results = theilsen(y, alpha=0.95) # p < 0.05
+        x = data_annual.index.to_julian_date() # list(range(1, len(data_annual) + 1))
+        data_annual['{}_trdmed'.format(col)] = theilsen_results['median_interc'] + theilsen_results['median_slope'] * x
+        data_annual['{}_trdlow'.format(col)] = theilsen_results['lower_CI_interc'] + theilsen_results['lower_CI_slope'] * x
+        data_annual['{}_trdupp'.format(col)] = theilsen_results['upper_CI_interc'] + theilsen_results['upper_CI_slope'] * x
+
+        # compare trends
+        plt.figure()
+        data_annual[col].plot(color="black", label='Sea-level anomaly')
+        data_annual["{}_trd_ols".format(col)].plot(color="red",
+                                                   label=r'OLS ($r^2={:.2f}, p={:.3f}$)'.format(trd.rsquared,
+                                                                                                trd.pvalues['x1']))
+        data_annual['{}_trdmed'.format(col)].plot(color="blue", label='Theilsen ()')
+        data_annual['{}_trdlow'.format(col)].plot(
+            color="blue", linestyle='dashed', label='Lower CI')
+        data_annual['{}_trdupp'.format(col)].plot(
+            color="blue", linestyle='dashed', label='Upper CI')
+
+        # figure aesthetics
+        sns.despine()
+        plt.title("Sea-level anomalies and trends: {}".format(col))
+        plt.ylabel("Sea level anomaly (m)")
+
+        plt.ylim(-0.05, 0.08)
+        if col == 'Nino34':
+            plt.ylim(-0.08, 0.2)
+
+        plt.grid(color='grey', linestyle='--', linewidth=0.5)
+        plt.legend(loc='upper left')
+        plt.savefig(os.path.join(outpath, 'ex1_sealevel_trend_{}.png'.format(col)))
+        plt.close()
+
+def plot_sst(fn_temp, outpath):
+    """
+    Visualize sealevel data and trends.
+
+    Parameters
+    ----------
+    fn_temp : str
+        Name of SST data file.
+    outpath : str
+        Name of output directory.
+    """
+    # create output directory if it does not yet exist
+    try:
+        os.makedirs(outpath)
+    except FileExistsError:
+        pass
+
+    # read sealevel data
+    data = read_data(infile=fn_temp, parse_col='Year', date_str='%Y')
+    data = data.drop(['Year'], axis=1)
+    print(data.columns)
+
+    # plot the data
+    plt.figure(figsize=(12,5))
+    columns = ['Glob', 'NHem', 'SHem']
+    plt.plot(data[columns])
+    sns.despine()
+    plt.title("Global SST anomalies")
+    plt.ylabel("SST anomaly (°C)")
+    plt.grid(color='grey', linestyle='--', linewidth=0.5)
+    plt.legend(columns, bbox_to_anchor=(1.01, 0.5), loc="center left")
+    plt.tight_layout()
+    plt.savefig(os.path.join(outpath, 'ex1_sst-global-anomalies-ts.png'))
+
+    # plot the data
+    plt.figure(figsize=(12,5))
+    columns_zonal = ['24N-90N', '24S-24N', '90S-24S', '64N-90N','44N-64N',
+                     '24N-44N', 'EQU-24N', '24S-EQU', '44S-24S', '64S-44S','90S-64S']
+    plt.plot(data[columns_zonal])
+    sns.despine()
+    plt.title("Latitudinal band SST anomalies")
+    plt.ylabel("SST anomaly (°C)")
+    plt.grid(color='grey', linestyle='--', linewidth=0.5)
+    plt.legend(columns_zonal, bbox_to_anchor=(1.01, 0.5), loc="center left")
+    plt.tight_layout()
+    plt.savefig(os.path.join(outpath, 'ex1_sst-zonal-anomalies-ts.png'))
+
 
     # aggregate to annual data
     data_annual = data.resample('A').mean()
@@ -116,22 +220,25 @@ def plot_sealevel(fn_sealevel, outpath):
 
         # compare trends
         plt.figure()
-        data_annual[col].plot(color="black", label='Sea-level anomaly')
-        data_annual["{}_trd_ols".format(col)].plot(color="red", label='OLS trend')
-        data_annual['{}_trdmed'.format(col)].plot(color="blue", label='Theil-Slope median trend')
+        data_annual[col].plot(color="black", label='SST anomaly')
+        data_annual["{}_trd_ols".format(col)].plot(color="red",
+                                                   label=r'OLS ($r^2={:.2f}, p={:.3f}$)'.format(trd.rsquared,
+                                                                                                trd.pvalues['x1']))
+        data_annual['{}_trdmed'.format(col)].plot(color="blue", label='Theilsen ()')
         data_annual['{}_trdlow'.format(col)].plot(
             color="blue", linestyle='dashed', label='Lower CI')
         data_annual['{}_trdupp'.format(col)].plot(
             color="blue", linestyle='dashed', label='Upper CI')
-        plt.title("Sea-level anomalies and trends: {}".format(col))
-        plt.ylabel("Sea level anomaly (m)")
 
-        plt.ylim(-0.05, 0.08)
-        if col == 'Nino34':
-            plt.ylim(-0.08, 0.2)
-        plt.grid(True)
+        # figure aesthetics
+        sns.despine()
+        plt.ylim(-2.5, 3)
+        plt.title("SST anomalies and trends: {}".format(col))
+        plt.ylabel("SST anomaly (°C)")
+        plt.grid(color='grey', linestyle='--', linewidth=0.5)
         plt.legend(loc='upper left')
-        plt.savefig(os.path.join(outpath, 'ex1_sealevel_trend_{}.png'.format(col)))
+        plt.tight_layout()
+        plt.savefig(os.path.join(outpath, 'ex1_sst_trend_{}.png'.format(col)))
         plt.close()
 
 
@@ -215,7 +322,12 @@ if __name__ == '__main__':
 
 
     # run the individual analyses
-    plot_sealevel(fn_sealevel, outpath)
+
+    # Analysis 1a: Trends in sea level anomalies
+    plot_sealevel(fn_sealevel, os.path.join(outpath, 'sealevel'))
+
+    # Analysis 1b: Trends in surface temperature anomalies
+    plot_sst(fn_temp, os.path.join(outpath, 'temperature'))
 
     #trend_analysis(fn_sealevel, fn_temp, fn_soi, outpath)
 
